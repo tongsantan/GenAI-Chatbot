419bc799bd31d8e219e013c1e770f1570fe1aa02
tongsantan <vincente53@outlook.com>
2024-07-02 15:39:52 +0800

    updated

--- app.py
+++ app.py
@@ -1,54 +1,109 @@
-import os
-from flask import Flask, render_template, request
-from src.utils import download_hugging_face_embeddings, load_pdf, text_split
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
 from langchain_community.llms import CTransformers
 from langchain_community.vectorstores import Chroma
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from src.utils import download_hugging_face_embeddings, text_split
+import os
+import tempfile
 
-app = Flask(__name__)
 
-extracted_data = load_pdf("./notebook/data/")
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
 
-text_chunks = text_split(extracted_data)
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! Ask me anything about the topic!"]
 
-embeddings = download_hugging_face_embeddings()
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey!"]
 
-#Loading the index
-vectorstore = Chroma.from_documents(text_chunks,embeddings)
+def conversation_chat(query, chain, history):
+    result = chain.invoke({"question": query, "chat_history": history})
+    history.append((query, result["answer"]))
+    return result["answer"]
 
-llm=CTransformers(model="./model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-                  model_type="llama",
-                  config={'max_new_tokens':512,
-                          'temperature':0.8})
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
 
-system_prompt = (
-    "Use the given context to answer the question. "
-    "If you don't know the answer, say you don't know. "
-    "Use three sentence maximum and keep the answer concise. "
-    "Context: {context}"
-)
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Question:", placeholder="Ask about your Documents", key='input')
+            submit_button = st.form_submit_button(label='Send')
 
-retriever = vectorstore.as_retriever(search_kwargs={'k': 3})
-document_chain = create_stuff_documents_chain(llm, prompt)
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        if submit_button and user_input:
+            with st.spinner('Generating response...'):
+                output = conversation_chat(user_input, chain, st.session_state['history'])
 
-@app.route("/")
-def index():
-    return render_template('chatbot.html')
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
 
-@app.route("/get", methods=["GET", "POST"])
-def chat():
-    msg = request.form["msg"]
-    response = retrieval_chain.invoke({"input": msg})
-    return str(response)
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+def create_conversational_chain(vectorstore):
+    # Create llm
+    llm = CTransformers(model="./model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                        streaming=True, 
+                        callbacks=[StreamingStdOutCallbackHandler()],
+                        model_type="llama", config={'max_new_tokens': 500, 'temperature': 0.01})
+    
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
+                                                 retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
+                                                 memory=memory)
+    return chain
+
+def main():
+    # Initialize session state
+    initialize_session_state()
+    st.title("Docs-query ChatBot")
+    # Initialize Streamlit
+    st.sidebar.title("Document Processing")
+    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
+
+
+    if uploaded_files:
+        text = []
+        for file in uploaded_files:
+            file_extension = os.path.splitext(file.name)[1]
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(file.read())
+                temp_file_path = temp_file.name
+
+            loader = None
+            if file_extension == ".pdf":
+                loader = PyPDFLoader(temp_file_path)
+            elif file_extension == ".docx" or file_extension == ".doc":
+                loader = Docx2txtLoader(temp_file_path)
+            elif file_extension == ".txt":
+                loader = TextLoader(temp_file_path)
+
+            if loader:
+                text.extend(loader.load())
+                os.remove(temp_file_path)
+
+        text_chunks = text_split(text)
+
+        # Create embeddings
+        embeddings = download_hugging_face_embeddings()
+
+        # Create vector store
+        vectorstore = Chroma.from_documents(text_chunks, embedding=embeddings)
+
+        # Create the chain object
+        chain = create_conversational_chain(vectorstore)
+
+        display_chat_history(chain)
+
+if __name__ == "__main__":
+    main()
+
